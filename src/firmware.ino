f1119322ad304dff0a068ff1deec0a1e4c22f145
doebi <christoph@doeberl.at>
2015-12-29 03:15:49 +0100

    added fancy web controls

--- src/firmware.ino
+++ src/firmware.ino
@@ -11,13 +11,13 @@
 String nodeName = "devlol/babyscooter/";
 
 // pins
-const int motor = 5;
+const int left = 14;
+const int right = 15;
 
-int motorState = LOW;
-int motorStart = 0;
-
-int isAlive = millis();
+int isAlive = 0;
 int mil = 0;
+
+char state = 0;
 
 ESP8266WiFiMulti wifiMulti;
 
@@ -26,61 +26,52 @@ WiFiClient wclient;
 PubSubClient client(wclient, MQTTserver);
 
 void mqtt_callback(const MQTT::Publish& pub) {
-  String topic = pub.topic();
-  String msg = pub.payload_string();
-  if(topic == "doebi/watergun/button") {
-    if (msg == "DOWN") {
-      motorStart = millis();
-      motorState = HIGH;
-      digitalWrite(motor, HIGH);
+    String topic = pub.topic();
+    String msg = pub.payload_string();
+    if(topic == "devlol/babyscooter/left") {
+        digitalWrite(left, msg == "0" ? LOW : HIGH);
     }
-    if (msg == "UP ") {
-      motorState = LOW;
-      digitalWrite(motor, LOW);
+    if(topic == "devlol/babyscooter/right") {
+        digitalWrite(right, msg == "0" ? LOW : HIGH);
     }
-  }
 }
 
 void setup() {
     Serial.begin(115200);
     delay(10);
-	
+
+    isAlive = millis();
+
     wifiMulti.addAP("/dev/lol", "4dprinter");
     wifiMulti.addAP("ESP-Spielwiese", "ovomaltine");
     wifiMulti.addAP("DildoAP", "dildo123");
     wifiMulti.addAP("32C3-open-legacy", "");
-    
+
     if(wifiMulti.run() == WL_CONNECTED) {
-      Serial.println("Wifi connected");
+        Serial.println("Wifi connected");
     }
-    
-    pinMode(motor, OUTPUT);
+
+    pinMode(left, OUTPUT);
+    pinMode(right, OUTPUT);
 }
 
 void loop() {
-  // heartbeat
-  mil = millis();
-  if(mil - isAlive >= 30000) {
-    client.publish(nodeName + "heartbeat", (String)mil);
-    isAlive = mil;  
-  }
-  
-  // wifi
-  if(WiFi.status() != WL_CONNECTED) {
-    if(wifiMulti.run() == WL_CONNECTED) {
-      Serial.println("Wifi connected");
+    // wifi
+    if(WiFi.status() != WL_CONNECTED) {
+        if(wifiMulti.run() == WL_CONNECTED) {
+            Serial.println("Wifi connected");
+        }
     }
-  }
 
     // mqtt
     if (client.connected()) {
-      client.loop();
+        client.loop();
     } else {
-      if (client.connect("babyscooter", nodeName + "online", 0, true, "false")) {
-        client.publish(nodeName + "online", "true", true);
-        Serial.println("MQTT connected");
-	      client.set_callback(mqtt_callback);
-	      client.subscribe("#");
-      }
+        if (client.connect("babyscooter", nodeName + "online", 0, true, "false")) {
+            client.publish(nodeName + "online", "true", true);
+            Serial.println("MQTT connected");
+            client.set_callback(mqtt_callback);
+            client.subscribe("#");
+        }
     }
 }
---
+++ web/js/app.js
@@ -0,0 +1,87 @@
+var mqtt;
+var reconnectTimeout = 2000;
+var host = "158.255.212.248";
+var port = 8080;
+var topic = "devlol/#";
+
+window.onkeydown = function(e) {
+    if (e.keyCode == 37 /* left */) {
+        setMotor('left', '100');
+        setMotor('right', '0');
+    }
+    if (e.keyCode == 39 /* right */) {
+        setMotor('right', '100');
+        setMotor('left', '0');
+    }
+    if (e.keyCode == 38 /* straight */) {
+        setMotor('left', '100');
+        setMotor('right', '100');
+    }
+    if (e.keyCode == 40 /* halt */) {
+        setMotor('right', '0');
+        setMotor('left', '0');
+    }
+}
+
+function setMotor(motor, value) {
+    mqtt.send("devlol/babyscooter/" + motor, value);
+}
+
+function MQTTconnect() {
+    console.log("connecting");
+    mqtt = new Paho.MQTT.Client(
+                    host,
+                    port,
+                    "web_" + parseInt(Math.random() * 100,
+                    10));
+    var options = {
+        timeout: 3,
+        onSuccess: onConnect,
+        onFailure: function (message) {
+            setTimeout(MQTTconnect, reconnectTimeout);
+        }
+    };
+
+    mqtt.onConnectionLost = onConnectionLost;
+    mqtt.onMessageArrived = onMessageArrived;
+
+    console.log("Host="+ host + ", port=" + port);
+    mqtt.connect(options);
+}
+
+function onConnect() {
+    console.log('Connected to ' + host + ':' + port);
+    mqtt.subscribe(topic, {qos: 0});
+    console.log(topic);
+}
+
+function onConnectionLost(response) {
+    setTimeout(MQTTconnect, reconnectTimeout);
+    console.log("connection lost: " + response.errorMessage + ". Reconnecting");
+};
+
+function onMessageArrived(message) {
+
+    var topic = message.destinationName;
+    var payload = message.payloadString;
+
+    console.log(topic + ' = ' + payload);
+
+    switch (topic) {
+        case 'doebi/watergun/heartbeat':
+            document.getElementById("uptime").innerHTML = moment(moment().valueOf() - Number(payload)).toNow(true);            
+            break;
+        case 'doebi/watergun/button':
+            document.getElementById("trigger").innerHTML = payload;            
+            break;
+        default:
+            break;
+    }
+};
+
+
+window.onload = function() {
+    console.log("booting up");
+    MQTTconnect();
+};
+
